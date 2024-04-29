deb9627bff94d3a0d175b30f977ff88b741e3626
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-29 16:19:37 +0900

    탁구공 자동 스포너 완성, 리깅 넣기
    - 탁구공이 자동으로 스폰됨.
    - 패키지 매니져에서 애니메이션 리깅 넣기

---
+++ MLAgent_Project/Assets/1. Scripts/Ball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+/*    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        Time.timeScale = 0.1f;
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.001f);
+        Debug.Log(rb.velocity);
+    }*/
+}
---
+++ MLAgent_Project/Assets/1. Scripts/BallSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ball;
+
+    private void Awake()
+    {
+        Time.timeScale = 0.5f;
+    }
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            GameObject throwBall = Instantiate(ball, transform.position, Quaternion.identity, transform);
+            Rigidbody ballRigidbody = throwBall.GetComponent<Rigidbody>();
+
+            float angle = Random.Range(-15f, 15f);      // 회전값
+            float zForce, yForce;  // 탁구대에서 앞으로 가는 힘 -40쪽이 적은 힘. 절댓값.    // 위로 가는 힘
+
+            zForce = Mathf.Lerp(-55f, -45f, angle / 15f);
+            yForce = Mathf.Lerp(35f, 25f, angle / 15f);
+
+            ballRigidbody.AddForce(new Vector3(zForce, yForce, angle));
+            Debug.Log($"앞으로 {zForce}만큼, 위로 {yForce}만큼, 각도는 {angle}임.");
+
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+}
+
+
+/*
+
+* 회전값 15일 때
+둘다 많을 때 앞으로 -50, 위로 30까지 가능
+둘다 적을 때 앞으로 -45, 위로 25까지 가능
+
+* 회전값 0일 때
+둘다 많을 때 앞으로 -55, 위로 35까지 가능
+둘다 적을 때 앞으로 -45, 위로 25까지 가능
+
+*/
